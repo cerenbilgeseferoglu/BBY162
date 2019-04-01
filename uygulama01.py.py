f2d1b7eca0e92eeb1ad436274caff82f2cec8793
cerenbilgeseferoglu <48280989+cerenbilgeseferoglu@users.noreply.github.com>
2019-04-01 14:49:30 +0300

    Add files via upload

---
+++ uygulama01.py.py
@@ -0,0 +1,60 @@
+sonuc = 0
+
+print("121 + 86 kaçtır? ")
+cevap = int(input( "Cevabınızı Giriniz: "))
+
+if (cevap == 207):
+    print("Tebrikler Doğru Cevap!")
+    sonuc = sonuc + 20
+else:
+    print("Maalesef Yanlış Cevap!")
+
+
+print("10 * 7 kaçtır? ")
+cevap = int(input( "Cevabınızı Giriniz: "))
+
+if (cevap == 70):
+    print("Tebrikler Doğru Cevap!")
+    sonuc = sonuc + 20
+else:
+    print("Maalesef Yanlış Cevap!")
+
+
+print("100 - 55 kaçtır? ")
+cevap = int(input( "Cevabınızı Giriniz: "))
+
+if (cevap == 45):
+    print("Tebrikler Doğru Cevap!")
+    sonuc = sonuc + 20
+else:
+    print("Maalesef Yanlış Cevap!")
+
+
+print("75 / 3 kaçtır? ")
+cevap = int(input( "Cevabınızı Giriniz: "))
+
+if (cevap == 25):
+    print("Tebrikler Doğru Cevap!")
+    sonuc = sonuc + 20
+else:
+    print("Maalesef Yanlış Ceva!")
+
+
+print("36 + 44 kaçtır? ")
+cevap = int(input( "Cevabınızı Giriniz: "))
+
+if (cevap == 80):
+    print("Tebrikler Doğru Cevap!")
+    sonuc = sonuc + 20
+else:
+    print("Maalesef Yanlış Cevap!")
+
+print("Aldığınız Toplam Puan =" , sonuc)
+
+
+
+
+
+
+
+
